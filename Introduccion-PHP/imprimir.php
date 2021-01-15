b0dbb6e76a1207dc38a82803fc910d1cfabd8c2a
paco-coral <franciscoalexander83@gmail.com>
2021-01-15 11:40:44 -0500

    Integramos la pagina imprimir .php

---
+++ Introduccion-PHP/imprimir.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Imprimir en PHP</title>
+</head>
+<body>
+<h1>Master en PHP</h1>
+
+<?="Atajo de texto <br>"?>
+
+<?php
+echo 'Hola mundo';
+echo " <ul>
+ <li>GTA</li>
+ <li>Fifa</li>
+ <li>Mario Bros</li>
+</ul>";
+echo " <br> ";
+echo " <p>Esto es un parraffo a mostrar</p> ";
+?>
+
+</body>
+</html>
